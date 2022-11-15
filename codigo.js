0132d156313fc0548ea1c116ec00a988b9c42b17
tautiu <alextautiucosigua@gmail.com>
2022-11-14 20:22:25 -0600

    first commit

---
+++ codigo.js
@@ -0,0 +1,6 @@
+'use strict'
+
+let numero1 = 5
+let numero2 = 'Axel'
+let suma = numero1 + numero2
+console.log(suma)
---
+++ temario.js
@@ -0,0 +1,9 @@
+let patas =5;
+let accion = "Ladra";
+
+/* if (patas ===  4 && accion = "Ladra"){
+    console.log("es un perro");
+}*/
+//"parte 1"? "parte 2": "parte 3"
+(patas ==4) && (accion == "Ladra")? 
+console.log("Es un perro");
